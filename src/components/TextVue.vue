9c08df4533481383d6ee01e8f174051a45eda581
youhui1214 <405852890@qq.com>
2021-03-16 17:02:03 +0800

    feat: 复习基础知识点

---
+++ src/components/TextVue.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        
+        <p v-if="seen">看到我了</p>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TextVue',
+        data() {
+            return {
+                msg: 'fffff',
+                seen: true
+            }
+        }
+    }
+</script>
+
